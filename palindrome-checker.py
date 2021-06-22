13accd7ba9b96fd9959689238edb121832ea362c
gbrough <gbrough@gmail.com>
2021-06-22 15:51:02 -0700

    add palindrome logic

--- palindrome-checker.py
+++ palindrome-checker.py
@@ -1,2 +1,13 @@
 # Ask user for input string
-# 
+# Reverse the string
+# compare if string is equal
+# challenge - use functions
+
+word = input("Please type a word you would like to see if it's a palindrome\n")
+reverseWord = word[::-1]
+
+if word == reverseWord:
+  print("Yes, you have entered a palindrome")
+else:
+  print("Sorry, this word is not a palindrome")
+# print(reverseWord)
